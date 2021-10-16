1fc700f5ac23f6ecdaa6e6eaa62001d979524517
CK-0428 <againnr1@gmail.com>
2021-10-17 08:37:30 +0900

    Get 방식 complete!

--- src/main/java/com/example/study/controller/GetController.java
+++ src/main/java/com/example/study/controller/GetController.java
@@ -1,8 +1,12 @@
 package com.example.study.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.study.model.SearchParam;
 
 @RestController
 @RequestMapping("/api")
@@ -13,5 +17,27 @@ public class GetController {
 	public String getRequest() {
 		
 		return "Hi getRequest!";
+	}
+	
+	
+	@GetMapping("/getParameter") //localhost:8080/api/getParameter?id=againnr&pwd=12345
+	public String getParameter(@RequestParam(name="idd") String id, 
+				@RequestParam(name="password") String pwd) {
+		String idd = "againnr1";
+		String password = "0852";
+		
+		
+		System.out.println("id: " + id);
+		System.out.println("pwd: " + pwd);
+		return id + " " + pwd;
+	}
+	
+	@GetMapping("/getMultiParameter")
+	public SearchParam getMultiParameter(SearchParam searchParam) {
+		System.out.println(searchParam.getAccount());
+		System.out.println(searchParam.getEmail());
+		System.out.println(searchParam.getPage());
+		
+		return searchParam;
 	}
 }
